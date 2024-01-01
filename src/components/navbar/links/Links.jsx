408178c2eea80f6785a5168377db833664ac2ebb
wukdddang <dnr8874@naver.com>
2024-01-02 00:12:38 +0900

    Feat: youtube 강의 학습 - 0:51 / 4:55 학습

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,47 @@
+import NavLink from "./navLink/navLink";
+
+import styles from "./links.module.css";
+
+const Links = () => {
+  const links = [
+    {
+      title: "Homepage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+  ];
+
+  // TEMPORARY
+  const session = true;
+  const isAdmin = true;
+
+  return (
+    <div>
+      {links.map((link) => (
+        <NavLink item={link} key={link.title} />
+      ))}
+
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "/login" }} />
+      )}
+    </div>
+  );
+};
+
+export default Links;
