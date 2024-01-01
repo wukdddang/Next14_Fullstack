6155ae02c888c6c1188956fb16e65db840e7550a
wukdddang <dnr8874@naver.com>
2024-01-01 23:08:03 +0900

    Chore: 프로젝트 세팅 수정 -> Nextjs14로 변경

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  images:{
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: "images.pexels.com"
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
