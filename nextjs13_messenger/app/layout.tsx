3338b2314c7fe1b2ef264bac865df356b4ec133e
wukdddang <dnr8874@naver.com>
2024-01-03 23:35:16 +0900

    Feat: Nextjs13 메신저 앱 강의 학습 - 1:17:58 / 8:49:13

---
+++ nextjs13_messenger/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Messenger Clone",
+  description: "Messenger Clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
