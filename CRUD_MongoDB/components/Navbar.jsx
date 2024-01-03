16834d0e0da873269eb1a3612820e6edf922d54f
wukdddang <dnr8874@naver.com>
2024-01-03 13:00:53 +0900

    Chore: Nextjs 프로젝트 추가

---
+++ CRUD_MongoDB/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link href={"/"}>Changuk</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
