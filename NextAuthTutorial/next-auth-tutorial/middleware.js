b2a0f43b12dc7cc493b16ffeb49c9d06cc33de92
wukdddang <dnr8874@naver.com>
2024-01-19 17:21:19 +0900

    Feat: 사내 기능 검토 - req.user에 user, email 데이터 집어넣기

--- NextAuthTutorial/next-auth-tutorial/middleware.js
+++ NextAuthTutorial/next-auth-tutorial/middleware.js
@@ -8,6 +8,9 @@ export default withAuth(
     console.log(req.nextauth.token.role);
     console.log(req.nextUrl.pathname.startsWith("/CreateUser"));
 
+    // TODO: req.user에 user와 email 정보를 담아서 넘겨주기.
+    req.user = req.nextauth.token;
+    console.log(req.user);
     if (
       req.nextUrl.pathname.startsWith("/CreateUser") &&
       req.nextauth.token.role !== "admin"
